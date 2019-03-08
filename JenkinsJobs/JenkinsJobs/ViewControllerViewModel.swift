d56656d808f2455e68399585ac8f05193906dec7
Serhat Sezer <serhat.szr@gmail.com>
2019-03-08 23:34:24 +0200

    added view model for controller

---
+++ JenkinsJobs/JenkinsJobs/ViewControllerViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerViewModel.swift
+//  JenkinsJobs
+//
+//  Created by Serhat Sezer on 8.03.2019.
+//  Copyright © 2019 Serhat Sezer. All rights reserved.
+//
+
+import Foundation
+
+struct ViewControllerViewModel {
+  func getTitle() -> String {
+    return "Hello world!"
+  }
+}
