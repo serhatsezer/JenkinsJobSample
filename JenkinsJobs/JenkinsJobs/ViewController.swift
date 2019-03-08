dee8cd6d32b02da590083242698ba0328f9b05ca
Serhat Sezer <serhat.szr@gmail.com>
2019-03-08 23:23:11 +0200

    initial

---
+++ JenkinsJobs/JenkinsJobs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsJobs
+//
+//  Created by Serhat Sezer on 8.03.2019.
+//  Copyright © 2019 Serhat Sezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
